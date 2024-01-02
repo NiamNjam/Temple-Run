30c467593b352ea3c0eca4da5cc2b1565bdea05f
NiamNjam <146223019+NiamNjam@users.noreply.github.com>
2024-01-03 00:42:59 +0200

    Character

---
+++ Assets/Scripts/TrailCamera.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailCamera : MonoBehaviour
+{
+    public Transform target;
+    float trailDistance = 5.0f;
+    float heightOffset = 3.0f;
+    float cameraDelay = 0.02f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 followPos = target.position - target.forward * trailDistance;
+
+        followPos.y += heightOffset;
+        transform.position += (followPos - transform.position) * cameraDelay;
+
+        transform.LookAt(target.transform);
+    }
+}
