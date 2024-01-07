744ac7a6e1c68be973230d5acc4a6eb18ed4b49c
NV <nida.kirdulyte@gmail.com>
2024-01-07 13:19:44 +0200

    running

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -7,31 +7,32 @@ public class PlayerMove : MonoBehaviour
     private bool turnLeft, turnRight;
     public float speed = 7.0f;
     private Rigidbody playerRigidbody;
-    //public string verticalAxis;
-    //public string horizontalAxis;
-    // Start is called before the first frame update
+    private CharacterController playerController;
+
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody>();
         playerRigidbody.freezeRotation = true;
+        //playerController = GetComponent<CharacterController>();
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
         turnLeft = Input.GetKeyDown(KeyCode.A);
         turnRight = Input.GetKeyDown(KeyCode.D);
 
         if (turnLeft)
-            playerRigidbody.transform.Rotate(new Vector3(0f, -90f, 0f));
+            transform.Rotate(new Vector3(0f, -90f, 0f));
         else if (turnRight)
-            playerRigidbody.transform.Rotate(new Vector3(0f, 90f, 0f));
+            transform.Rotate(new Vector3(0f, 90f, 0f));
 
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
-        movement = transform.TransformDirection(movement); // Convert local to global space
-        playerRigidbody.velocity = movement * speed * Time.deltaTime;
+        //playerController.SimpleMove(new Vector3(0f, 0f, 0f));
+        movement = transform.TransformDirection(movement);
+        playerRigidbody.Move(movement * speed * Time.deltaTime);
     }
 }
