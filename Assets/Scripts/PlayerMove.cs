dd63056ed0687dd72ffc1a9ed8d0e129c374da4f
NiamNjam <146223019+NiamNjam@users.noreply.github.com>
2024-01-07 22:54:52 +0200

    running

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -6,14 +6,16 @@ public class PlayerMove : MonoBehaviour
 {
     private bool turnLeft, turnRight;
     public float speed = 7.0f;
-    private Rigidbody playerRigidbody;
+    //private Rigidbody playerRigidbody;
+    private CharacterController myCharacter;
     //public string verticalAxis;
     //public string horizontalAxis;
     // Start is called before the first frame update
     void Start()
     {
-        playerRigidbody = GetComponent<Rigidbody>();
-        playerRigidbody.freezeRotation = true;
+        //playerRigidbody = GetComponent<Rigidbody>();
+        //playerRigidbody.freezeRotation = true;
+        myCharacter = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
@@ -23,15 +25,16 @@ public class PlayerMove : MonoBehaviour
         turnRight = Input.GetKeyDown(KeyCode.D);
 
         if (turnLeft)
-            playerRigidbody.transform.Rotate(new Vector3(0f, -90f, 0f));
+            myCharacter.transform.Rotate(new Vector3(0f, -90f, 0f));
         else if (turnRight)
-            playerRigidbody.transform.Rotate(new Vector3(0f, 90f, 0f));
+            myCharacter.transform.Rotate(new Vector3(0f, 90f, 0f));
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontalInput, 0f, verticalInput);
-        movement = transform.TransformDirection(movement); // Convert local to global space
-        playerRigidbody.velocity = movement * speed * Time.deltaTime;
+        //Vector3 movement = new Vector3(0f, 0f, 0f);
+        //movement = transform.TransformDirection(movement); // Convert local to global space
+        myCharacter.SimpleMove(new Vector3(0f, 0f, 0f));
+        myCharacter.Move(transform.forward * speed * Time.deltaTime);
     }
 }
